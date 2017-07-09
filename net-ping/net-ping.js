163a6412a0dd1b5734fe2eb8653296a267f72b9a
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2017-07-09 18:34:56 -0400

    Working example

--- net-ping/net-ping.js
+++ net-ping/net-ping.js
@@ -1,13 +1,27 @@
 const ping = require('net-ping');
 
 module.exports = function(RED) {
-  function NetPingNode(config) {
-    RED.nodes.createNode(this, config);
-    var node = this;
-    this.on('input', function(msg) {
-      msg.payload = msg.payload.toLowerCase();
-      node.send(msg);
-    });
-  }
-  RED.nodes.registerType("net-ping-node", NetPingNode);
+    function NetPingNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        this.on('input', function(msg) {
+            const options = {
+                retries: 1,
+                timeout: 2000
+            };
+            const session = ping.createSession(options);
+            session.on('error', (error) => node.error(error));
+            session.pingHost(msg.payload, (error, target) => {
+                if (error) {
+                    node.warn(error);
+                } else {
+                    node.warn(`Success ${target}`);
+                }
+            });
+            //msg.payload = msg.payload.toLowerCase();
+            //node.send(msg);
+        });
+    }
+    RED.nodes.registerType("net-ping-node", NetPingNode);
 };
